b3c61c8acf7a6410acf5457006770773ab57a635
Saiful Aziz <gamestopik712@gmail.com>
2026-06-12 16:43:24 +0500

    feat: implemented create Book business logic

--- src/main/java/com/bookstore/api/service/impl/BookServiceImpl.java
+++ src/main/java/com/bookstore/api/service/impl/BookServiceImpl.java
@@ -1,4 +1,53 @@
 package com.bookstore.api.service.impl;
 
-public class BookServiceImpl {
+import com.bookstore.api.Repository.BookRepository;
+import com.bookstore.api.dto.BookRequestDTO;
+import com.bookstore.api.dto.BookResponseDTO;
+import com.bookstore.api.exception.DuplicateResourceException;
+import com.bookstore.api.mapper.BookMapper;
+import com.bookstore.api.model.Book;
+import com.bookstore.api.service.BookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BookServiceImpl implements BookService {
+
+    private final BookRepository bookRepo;
+    private final BookMapper bookMapper;
+
+    @Override
+    public BookResponseDTO createBook(BookRequestDTO dto) {
+
+        if (bookRepo.existsByIsbn(dto.isbn())) {
+            throw new DuplicateResourceException("A book with ISBN " + dto.isbn() + " already exists.");
+        }
+
+        Book book = bookMapper.toEntity(dto);
+        Book savedBook = bookRepo.save(book);
+        return bookMapper.toResponseDTO(savedBook);
+    }
+
+    @Override
+    public List<BookResponseDTO> getAllBooks() {
+        return List.of();
+    }
+
+    @Override
+    public BookResponseDTO getBookById(String id) {
+        return null;
+    }
+
+    @Override
+    public BookResponseDTO updateBook(String id, BookRequestDTO dto) {
+        return null;
+    }
+
+    @Override
+    public void deleteBook(String id) {
+
+    }
 }
