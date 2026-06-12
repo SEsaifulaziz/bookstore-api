4d3747e12b77e620d1a34e25b1ff3a159bbc2c68
Saiful Aziz <gamestopik712@gmail.com>
2026-06-12 16:31:59 +0500

    feat: add a ResourceNotFoundException class

---
+++ src/main/java/com/bookstore/api/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.bookstore.api.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
