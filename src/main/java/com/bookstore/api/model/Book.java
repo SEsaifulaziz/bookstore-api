fbac85b128287045901487c0007d5014cf61facc
Saiful Aziz <gamestopik712@gmail.com>
2026-06-12 16:06:06 +0500

    feat: implement MongoDB Book document entity with unique ISBN indexing

---
+++ src/main/java/com/bookstore/api/model/Book.java
@@ -0,0 +1,30 @@
+package com.bookstore.api.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collation = "books")
+public class Book {
+    @Id
+    private String id;
+
+    private String title;
+    private String author;
+    private BigDecimal price;
+
+    @Indexed(unique = true)
+    private String isbn;
+
+    private LocalDate publishedDate;
+}
