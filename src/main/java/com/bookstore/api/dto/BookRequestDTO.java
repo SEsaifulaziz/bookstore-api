8c878aea26e3f0952717c397ca6ea616ae3075f5
Saiful Aziz <gamestopik712@gmail.com>
2026-06-12 16:15:14 +0500

    feat: add BookResquestDTO with validation rules

--- src/main/java/com/bookstore/api/dto/BookRequestDTO.java
+++ src/main/java/com/bookstore/api/dto/BookRequestDTO.java
@@ -1,4 +1,28 @@
 package com.bookstore.api.dto;
 
-public class BookRequestDTO {
-}
+import jakarta.validation.constraints.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public record BookRequestDTO(
+        @NotBlank(message = "Title cannot be blank")
+        @Size(max = 100, message = "Title cannot exceed 100 characters")
+        String title,
+
+        @NotBlank(message = "Author cannot be blank")
+        @Size(max = 50, message = "Author cannot exceed 50 characters")
+        String author,
+
+        @NotNull(message = "Price cannot be null")
+        @Positive(message = "Price must be a positive value")
+        BigDecimal price,
+
+        @NotBlank(message = "ISBN cannot be blank")
+        @Size(min = 10, max = 13, message = "ISBN must be between 10 and 13 characters")
+        String isbn,
+
+        @NotNull(message = "Published date cannot be null")
+        @PastOrPresent(message = "Published date cannot be in the future")
+        LocalDate publishedDate
+) {}
