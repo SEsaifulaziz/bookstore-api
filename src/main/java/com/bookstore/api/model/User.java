0d06f64dc4ff5a00a3b60b31fc102e4f16ef2ef8
Saiful Aziz <gamestopik712@gmail.com>
2026-06-15 15:43:00 +0500

    feat: implement User document entity with an associated ownedBookIds reference list

---
+++ src/main/java/com/bookstore/api/model/User.java
@@ -0,0 +1,31 @@
+package com.bookstore.api.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Document(collection = "users")
+public class User {
+
+    @Id
+    private String id;
+
+    private String name;
+
+    @Indexed(unique = true)
+    private String email;
+
+    @Builder.Default
+    private List<String> ownedBookIds = new ArrayList<>();
+}
