bf6505ca245eea1aa82e2333de32ebf678ac8422
Saiful Aziz <gamestopik712@gmail.com>
2026-06-16 11:03:35 +0500

    Refactor: add pagination to get all users

--- src/main/java/com/bookstore/api/service/UserService.java
+++ src/main/java/com/bookstore/api/service/UserService.java
@@ -2,16 +2,15 @@ package com.bookstore.api.service;
 
 import com.bookstore.api.dto.request.UserRequestDTO;
 import com.bookstore.api.dto.response.UserResponseDTO;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public interface UserService {
 
     UserResponseDTO createUser(UserRequestDTO userRequestDTO);
 
-    List<UserResponseDTO> getAllUsers();
+    Page<UserResponseDTO> getAllUsers(int page, int size);
 
     UserResponseDTO addBookToUser(String userId, String bookId);
 }
