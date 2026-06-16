937177a4155b976243c32608496f3db695be6cda
Saiful Aziz <gamestopik712@gmail.com>
2026-06-16 10:56:16 +0500

    feat: implement create user method

---
+++ src/main/java/com/bookstore/api/service/impl/UserServiceImpl.java
@@ -0,0 +1,48 @@
+package com.bookstore.api.service.impl;
+
+import com.bookstore.api.Repository.BookRepository;
+import com.bookstore.api.Repository.UserRepository;
+import com.bookstore.api.dto.request.UserRequestDTO;
+import com.bookstore.api.dto.response.UserResponseDTO;
+import com.bookstore.api.exception.DuplicateResourceException;
+import com.bookstore.api.mapper.UserMapper;
+import com.bookstore.api.model.User;
+import com.bookstore.api.service.BookService;
+import com.bookstore.api.service.UserService;
+import jdk.jshell.spi.ExecutionControl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepo;
+    private final BookRepository bookRepo;
+    private final UserMapper userMapper;
+
+    @Override
+    public UserResponseDTO createUser(UserRequestDTO userRequestDTO) {
+        if(userRepo.existsByEmail(userRequestDTO.email())){
+            throw new DuplicateResourceException("A user with this email already exists");
+        }
+
+        User user = userMapper.toEntity(userRequestDTO);
+        User savedUser = userRepo.save(user);
+        return userMapper.toResponseDTO(savedUser);
+    }
+
+    @Override
+    public List<UserResponseDTO> getAllUsers() {
+        return List.of();
+    }
+
+    @Override
+    public UserResponseDTO addBookToUser(String userId, String bookId) {
+        return null;
+    }
+
+
+}
